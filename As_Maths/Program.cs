df9ecd414127ca59fdd64466fe56c73b39956489
coditas <coditas@ANAPHADE-LAP-02>
2022-07-12 17:00:04 +0530

    Add project files.

---
+++ As_Maths/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fnum, snum, addn, subn, muln, divn;
+int flag = 1;
+int pow = 1;
+
+Console.WriteLine("Enter the first number");
+fnum= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+snum = Convert.ToInt32(Console.ReadLine());
+addn = fnum + snum;
+subn = fnum - snum;
+muln = fnum * snum;
+divn = fnum / snum;
+while (flag <= snum)
+{
+    pow = pow * fnum;
+    flag++;
+}
+Console.WriteLine($"Power is {pow}");
+Console.WriteLine($"Addition is {addn}");
+Console.WriteLine($"substraction is {subn}");
+Console.WriteLine($"Multiplication is {muln}");
+Console.WriteLine($"division is {divn}");
+Console.WriteLine($"Square of first number is {fnum * fnum} and square of second number is {snum * snum}");
+
+
+
+
+
---
+++ CS_DataTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// declare variable
+int x, y, z;
+Console.WriteLine("Enter x");
+x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y");
+y=Convert.ToInt32(Console.ReadLine());
+z = x + y;
+Console.WriteLine($"Value of z is {z} ");
